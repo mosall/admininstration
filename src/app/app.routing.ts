45a4f064f94477204361d79b0c164142821aba4e
Mouhamed Wade <mdwade37@gmail.com>
2021-11-15 12:28:24 +0000

    delete auth component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,16 +5,6 @@ export const AppRoutes: Routes = [
     path: '',
     children: [
       {
-        path: 'auth',
-        loadChildren: () =>
-          import('./components/auth/auth.module').then(m => m.AuthModule)
-      }
-    ]
-  },
-  {
-    path: '',
-    children: [
-      {
         path: 'ci-pme',
         loadChildren: () =>
           import('./components/ci-pme/ci-pme.module').then(m => m.CiPmeModule)
