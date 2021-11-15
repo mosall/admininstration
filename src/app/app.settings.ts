4c558971a2387a6d6bfc8ca90d23f1b29b6d8306
msall1006 <mohamaddou.sall@awaconsulting.com>
2021-11-15 17:04:09 +0000

    constant & config

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -2,10 +2,10 @@ import { HttpHeaders } from "@angular/common/http";
 
 export default class AppSettings {
 
-  private static BACK_URL = "http://217.182.185.176:3000";
+  private static BACK_URL = "http://localhost:3000";
 	 
-	public static UM_URL: string = AppSettings.BACK_URL+"/um/api/auth";
-	public static UM_TOKEN_URL: string = AppSettings.BACK_URL+"/um/oauth/token";
+	public static UM_URL: string = AppSettings.BACK_URL+"/administration/api/auth";
+	public static UM_TOKEN_URL: string = AppSettings.BACK_URL+"/administration/oauth/token";
 
 	public static httpOptions = {
       headers: new HttpHeaders({
