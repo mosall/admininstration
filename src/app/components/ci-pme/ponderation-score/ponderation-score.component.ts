f92d4874269aa25d18f365c1fa098c90ddb31bf2
Mouhamed Wade <mdwade37@gmail.com>
2021-11-29 17:41:03 +0000

    show ponderation

--- src/app/components/ci-pme/ponderation-score/ponderation-score.component.ts
+++ src/app/components/ci-pme/ponderation-score/ponderation-score.component.ts
@@ -1,8 +1,10 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {DataTableDirective} from "angular-datatables";
-import {Subject} from "rxjs";
-import {DatatableSettings} from "../../../settings/datatable.settings";
+import { DataTableDirective } from 'angular-datatables';
+import { Subject } from 'rxjs';
+import { DatatableSettings } from 'src/app/settings/datatable.settings';
+import Swal from 'sweetalert2';
+import {ScoreService} from "../../../services/score.service";
 declare var $: any;
 
 @Component({
@@ -18,48 +20,116 @@ export class PonderationScoreComponent implements OnInit {
 
   addScoreForm: FormGroup = new FormGroup({});
   submitted = false;
-  constructor(private formBuilder: FormBuilder) { }
+
+  listPonderation: any = [];
+  code: any = "";
+  idParametre: any = null;
+  valeur: any = null;
+  idPonderation: any = null;
+
+  constructor(private formBuilder: FormBuilder, private ponderationService: ScoreService) { }
 
   ngOnInit(): void {
     this.dtOptions = DatatableSettings.dataTableOptions();
-
-    this.initForms();
+    this.getPonderation();
   }
 
-  initForms(){
-    this.addScoreForm = this.formBuilder.group({
-      code: ['', Validators.required],
-      parameter: ['', Validators.required],
-      value: ['', Validators.required]
+  savePonderation(){
+    this.submitted = true;
+    console.log(this.idParametre);
+
+    if (this.code == '' || isNaN(this.idParametre) || this.idParametre == null || isNaN(this.valeur) || this.valeur == null ){
+      return;
+    }
+    else {
+      const data = {
+        code_score: this.code,
+        idParametre: this.idParametre,
+        ponderation: this.valeur
+      }
+
+
+
+      if(this.idPonderation != null){
+        // @ts-ignore
+        data.id = this.idPonderation;
+      }
+
+      // @ts-ignore
+      const msg = data.id ? "La pondération a été modifiée avec succès." : "La pondération a été enregistrée avec succès."
+
+      this.ponderationService.savePonderation(data).subscribe(
+        data => this.successMsgBox(msg),
+        error => this.errorMsgBox(error.error),
+      );
+
+
+
+    }
+  }
+
+  getPonderation(){
+    this.ponderationService.getPonderation().subscribe(
+      data => {
+        this.listPonderation = data;
+        console.log(data);
+        this.dtTrigger.next();
+      }
+    );
+  }
+
+  deletePonderation(idQuestion: any){
+    Swal.fire({
+      title: 'Suppression',
+      text: 'Êtes vous sûr de vouloir supprimer la pondération ?',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonColor: '#006a25',
+      cancelButtonColor: '#f78300',
+      confirmButtonText: 'Oui',
+      cancelButtonText: 'Annuler'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // tslint:disable-next-line:triple-equals
+        this.ponderationService.deletePonderation(idQuestion).subscribe(
+          data => this.successMsgBox("La pondération a été supprimée."),
+          error => this.errorMsgBox(error.error),
+        );
+      }
     });
   }
 
-  get q(){
-    return this.addScoreForm.controls;
+
+  onUpdateScoreClick(id: any){
+    const ponderation = this.listPonderation.find((param: { id: any; }) => param.id == id);
+    this.code = ponderation.code_score;
+    this.idParametre = ponderation.parametreDTO.id;
+    this.valeur = ponderation.ponderation;
+    this.idPonderation = id;
   }
 
-  saveScore(){
-    this.submitted = true;
-    if (this.addScoreForm.invalid){
-      return;
-    }
-
-    const data = {
-      code: this.addScoreForm.get('code')?.value,
-      parameter: this.addScoreForm.get('parameter')?.value,
-      value: this.addScoreForm.get('value')?.value,
-    }
-
-    console.log(data)
-
+  successMsgBox(msg: any){
+    Swal.fire({
+      icon: 'success',
+      text: msg,
+      showConfirmButton: false,
+      timer: 1500
+    }).then(
+      ()=> window.location.reload()
+    );
   }
 
-  getScore(idQuestion: any){}
+  errorMsgBox(msg: any){
+    Swal.fire({
+      icon: 'warning',
+      text: msg,
+      showConfirmButton: false,
+      timer: 2500
+    });
+  }
 
-  deleteScore(idQuestion: any){}
-
-  onUpdateScoreClick(){}
-
-  updateScore(){}
+  isNan(value: any){
+    return isNaN(value);
+  }
 
 }
--- src/app/services/score.service.ts
+++ src/app/services/score.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import {AppSettings} from "../settings/app.settings";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ScoreService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  savePonderation(payload: any){
+    return this.http.post(AppSettings.CIPME_ADMINISTRATION_API_URL + `/api/ponderations/create`, payload, AppSettings.httpOptions);
+  }
+
+  getPonderation(){
+    return this.http.get(AppSettings.CIPME_ADMINISTRATION_API_URL + `/api/ponderations`, AppSettings.httpOptions);
+  }
+
+  updatPonderationr(payload: any){
+    return this.http.put(AppSettings.CIPME_ADMINISTRATION_API_URL + `/api/ponderations/update`, payload, AppSettings.httpOptions);
+  }
+
+  deletePonderation(id: any){
+    return this.http.delete(AppSettings.CIPME_ADMINISTRATION_API_URL + `/api/ponderations/${id}/delete`, AppSettings.httpOptions);
+  }
 }
