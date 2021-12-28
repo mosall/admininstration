4c37f09c8c32ab0d6d30ddd5a77880df916f5cec
Mouhamed Wade <mdwade37@gmail.com>
2021-12-28 17:01:39 +0000

    hide code list question quali

--- src/app/settings/app.settings.ts
+++ src/app/settings/app.settings.ts
@@ -1,8 +1,8 @@
 import {HttpHeaders} from "@angular/common/http";
 
 export class AppSettings {
-  public static CIPME_ADMINISTRATION_API_URL    = 'http://localhost:3000/administration';
-  // public static CIPME_ADMINISTRATION_API_URL    = 'http://217.182.185.176:3000/administration';
+  // public static CIPME_ADMINISTRATION_API_URL    = 'http://localhost:3000/administration';
+  public static CIPME_ADMINISTRATION_API_URL    = 'http://217.182.185.176:3000/administration';
 
   public static httpOptions = {
     headers: new HttpHeaders({
