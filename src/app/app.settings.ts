581e94c3a2643fe28df89f756388fdaa0442e807
Mohamadou SALL <mohamaddou.sall@awaconsulting.com>
2022-03-31 16:04:22 +0000

    setup cipme deploy config

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -2,13 +2,13 @@ import { HttpHeaders } from "@angular/common/http";
 
 export default class AppSettings {
 
-  private static BACK_URL = "http://localhost";
+  private static BACK_URL = "http://217.182.185.176";
 	 
-	public static UM_URL: string = AppSettings.BACK_URL+":3000/administration/api/auth";
-	public static UM_REFERENTIEL: string = AppSettings.BACK_URL+":3000/administration/api";
-	public static UM_TOKEN_URL: string = AppSettings.BACK_URL+":3000/administration/oauth/token";
+	public static UM_URL: string = AppSettings.BACK_URL+":9000/administration/api/auth";
+	public static UM_REFERENTIEL: string = AppSettings.BACK_URL+":9000/administration/api";
+	public static UM_TOKEN_URL: string = AppSettings.BACK_URL+":9000/administration/oauth/token";
   
-	public static URL_SCORING_HOME: string = AppSettings.BACK_URL+"/scoring-awi/ci-pme";
+	public static URL_SCORING_HOME: string = AppSettings.BACK_URL+"/scoring/ci-pme";
 
 	public static httpOptions = {
       headers: new HttpHeaders({
