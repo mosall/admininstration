cbfe178606d51e853da7cf5275cab17fdb8d9a06
Mohamadou SALL <mohamaddou.sall@awaconsulting.com>
2022-05-05 10:51:55 +0000

    build: set environment variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,20 @@
+import { HttpHeaders } from "@angular/common/http";
+
+const BACK_URL: string = "http://217.182.185.176";
+
 export const environment = {
-  production: true
+  production: true,
+	 
+	UM_URL: BACK_URL+":9000/administration/api/auth",
+	UM_REFERENTIEL: BACK_URL+":9000/administration/api",
+	UM_TOKEN_URL: BACK_URL+":9000/administration/oauth/token",
+  
+	URL_SCORING_HOME: BACK_URL+"/scoring/ci-pme",
+
+	httpOptions : {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Access-Control-Allow-Origin': '*',
+      })
+    },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,25 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { HttpHeaders } from "@angular/common/http";
+
+const BACK_URL: string = "http://localhost";
+
 export const environment = {
-  production: false
+  production: false,
+
+  UM_URL: BACK_URL+":9000/administration/api/auth",
+	UM_REFERENTIEL: BACK_URL+":9000/administration/api",
+	UM_TOKEN_URL: BACK_URL+":9000/administration/oauth/token",
+  
+	URL_SCORING_HOME: BACK_URL+"/scoring/ci-pme",
+
+	httpOptions : {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Access-Control-Allow-Origin': '*',
+      })
+    },
 };
 
 /*
